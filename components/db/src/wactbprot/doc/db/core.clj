127500fdd4478f67d4bdf95ff7d9e28786a6c5b9
Wact. B. Prot <wactbprot@gmail.com>
2021-10-02 09:58:17 +0200

    some db core

--- components/db/src/wactbprot/doc/db/core.clj
+++ components/db/src/wactbprot/doc/db/core.clj
@@ -3,28 +3,31 @@
     :doc "Basic database interop. Plain HTTP."}
   (:require [cheshire.core :as che]
             [clojure.string :as string]
+            [wactbprot.doc.config.interface :as c]
             [org.httpkit.client :as http]))
-
-(defn result [{body :body status :status}]
-  (let [body (try
-               (che/parse-string-strict body true )
-               (catch Exception e
-                 {:error (.getMessage e)}))]
-    (if (< status 400)
-      body
-      {:error (:error body) :reason (:reason body)})))
-
-(defn get-rev [url opt]
-  (let [res @(http/head url opt)]
-    (when-not (contains? res :error)
-      (string/replace (get-in  [:headers :etag]) #"\"" ""))))
 
 (defn doc-url [{db-url :db-url rev :rev} id]
   (when (and db-url id) (str db-url "/" id (when rev (str "?rev=" rev)))))
 
+(defn result [{body :body status :status}]
+  (let [body (try (che/parse-string-strict body true )
+               (catch Exception e {:error (.getMessage e)}))]
+    (if (< status 400)
+      body
+      {:error (:error body) :reason (:reason body)})))
+
+(defn get-rev [{opt :db-opt :as conf} id]
+  (let [res @(http/head (doc-url conf id) opt)]
+    (when (< (:status res) 400)
+      (string/replace (get-in  res [:headers :etag]) #"\"" ""))))
+
 (defn get-doc [{opt :db-opt :as conf} id]
-  (result @(http/get (doc-url conf) opt)))
+  (result @(http/get (doc-url conf id) opt)))
 
 (defn del-doc [{opt :db-opt :as conf} id]
   (let [url (doc-url conf id)]
-  (result @(http/delete (doc-url (assoc conf :rev (get-rev url opt))) opt))))
+    (result @(http/delete (doc-url (assoc conf :rev (get-rev conf id)) id) opt))))
+
+(comment
+  (get-doc c/conf "foo")
+  (get-rev c/conf "000_REPLICATIONS"))
